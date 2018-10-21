bb29f87276af2fdac96d4a34a5f9441199a6cd01
Gökhan Orhun <gokhan@orhun.nl>
2018-10-21 22:38:49 +0200

    Create component menubar with some sample items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
